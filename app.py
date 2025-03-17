04954d8f17b9f1b09554847b6ffa65c32f0185a6
abelasdf <abelpsn98@gmail.com>
2025-03-17 20:28:07 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/nachricht")
+def nachricht():
+    return "Hallo, das ist eine Nachricht von Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
